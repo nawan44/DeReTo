6d31350cfbed48baf9075cd4644265b5ee895f5d
nawan44 <rachmat.d.gunawan@gmail.com>
2022-08-19 21:28:56 +0700

    data-cy=modal-delete

--- src/component/layout/cardActivity.js
+++ src/component/layout/cardActivity.js
@@ -49,8 +49,8 @@ export default function CardActivity(props) {
           {moment(item.created_at).format("DD MMMM  YYYY")}
         </Typography>
         <DeleteIcon
-
-                        data-cy="activity-item-delete-button"
+data-cy="modal-delete"
+                        // data-cy="activity-item-delete-button"
           onClick={(id) => handleDeleteList(item)}
           style={{ width: "20%", float: "right", color: "#888888" }}
         />
